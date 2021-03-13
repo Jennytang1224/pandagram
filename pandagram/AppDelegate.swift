4b61a2a3f3ffb5aa46d221ad1b1a779802cbb6a6
Jenny Tang <tangjenny1224@gmail.com>
2021-03-12 23:57:08 -0800

    need to watch one last video

--- pandagram/AppDelegate.swift
+++ pandagram/AppDelegate.swift
@@ -6,16 +6,31 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "GmAYXhxudg7TmEOnWrQ6hW3w4nFiOyYTzfvUfC66" // <- UPDATE
+                $0.clientKey = "zRgx2iK04lZYg5vU8YALXOVPa13aj6hJ8DL5T9fy" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+    
+        // --- end copy
+
         return true
     }
+//
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//        return true
+//    }
 
     // MARK: UISceneSession Lifecycle
 
